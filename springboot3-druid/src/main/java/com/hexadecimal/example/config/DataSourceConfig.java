f3ed3b681b35e5b9263ade238b54ce8195752a62
youjianping <youjianping@linklogis.com>
2023-06-10 22:53:08 +0800

    druid

--- springboot3-druid/src/main/java/com/hexadecimal/example/config/DataSourceConfig.java
+++ springboot3-druid/src/main/java/com/hexadecimal/example/config/DataSourceConfig.java
@@ -1,6 +1,6 @@
 package com.hexadecimal.example.config;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.spring.boot3.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
